de77632eb72390e7bd7d929c354ed50e785684bb
superleno4ka2007-hash <superleno4ka2007@gmail.com>
2026-02-05 17:57:42 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace GitHub_UserActivity
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
