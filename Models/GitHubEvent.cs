c92e80cd3575279964593c823294b2931c3b6dd9
superleno4ka2007-hash <superleno4ka2007@gmail.com>
2026-02-06 10:06:33 +0300

    before UI

---
+++ Models/GitHubEvent.cs
@@ -0,0 +1,45 @@
+﻿// описывает структуру json-ответа 
+
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.Xml;
+using System.Text;
+
+namespace GitHub_UserActivity.Models
+{
+    class GitHubEvent // представляет одно событие 
+    {
+        public string? Type { get; set; } // PushEvent, IssuesEvemt, WatchEvent
+        public PayLoad? PayLoad { get; set; } // дополнительные данные в зависимости от типа события 
+        public Repo? Repo { get; set; } 
+        public Actor? Actor { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+    public class PayLoad
+    {
+        public int? Size { get; set; } // количество коммитов 
+        public PushCommit[]? Commits { get; set; } // массив коммитов с сообщениями 
+        public Issue? Issue { get; set; } 
+    }
+
+    public class PushCommit
+    {
+        public string? Message { get; set; } // 
+    }
+
+    public class Issue
+    {
+        public string? Title { get; set; }
+    }
+
+    public class Repo
+    {
+        public string? Name { get; set; }
+    }
+
+    public class Autor
+    {
+        public string? Login { get; set; }
+    }
+}
---
+++ Services/GitHubService.cs
@@ -0,0 +1,70 @@
+﻿// взаимодействие с GitHub API
+
+using GitHub_UserActivity.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+
+namespace GitHub_UserActivity.Services
+{
+    class GitHubService
+    {
+        // 1 экземпляр на все приложение. HttpClient - отправка Http-запросов и получение ответов от веб-сервисов 
+        public static readonly HttpClient HttpClient = new HttpClient();
+
+        // настройка сопоставления json-файла c нашим классом. передаем в Deserialise параметры 
+        // PropertyNameCaseInsensitive = true - при поиске соответствий игнорируй регистр 
+        private static readonly JsonSerializerOptions JsonOptions = new() { PropertyNameCaseInsensitive = true };
+
+        // 1 кэш на все приложение 
+        // readonly гарантирует, что, к примеру, cache нельзя присвоить null
+        // ключ - string - имя пользователя. значение - DateTime, List<GitHubEvent> - кортеж (время сохранения данных в кэш, список событий)
+        private static readonly Dictionary<string, (DateTime, List<GitHubEvent>)> cache = new();
+
+        // время хранения в кэше 
+        private const int CacheMinutes = 5;
+
+        // метод получения событий 
+        public async Task<List<GitHubEvent>> GetUserEventsAsync(string username)
+        {
+            if (string.IsNullOrWhiteSpace(username)) {
+                throw new ArgumentNullException("username cannot be null or empty.");
+            }
+            var cacheKey = username.ToLowerInvariant(); // ключ для кэша
+
+            // проверка кэша
+            if (cache.TryGetValue(cacheKey, out var cached) && DateTime.UtcNow - cached.Item1 < TimeSpan.FromMinutes(CacheMinutes))
+            {
+                return cached.Item2;
+            }
+
+            // формирование url 
+            var url = $"https://api.github.com/users/{username}/events";
+            HttpClient.DefaultRequestHeaders.UserAgent.ParseAdd("GitHubActivityWpf/1.0");
+
+            // отправка запроса 
+            using var response = await HttpClient.GetAsync(url); // get, post, put
+
+            // если статус ответа не 2хх (404)
+            if (!response.IsSuccessStatusCode)
+                throw new InvalidOperationException($"Не удалось получить данные: {response.StatusCode}");
+            var json = await response.Content.ReadAsStringAsync();
+            List<GitHubEvent> events;
+            try
+            {
+                events = JsonSerializer.Deserialize<List<GitHubEvent>>(json, JsonOptions) ?? [];
+            }
+            catch (JsonException ex)
+            {
+                throw new InvalidOperationException($"Не удалось разобрать ответ от GitHub.");
+            }
+            cache[cacheKey] = (DateTime.UtcNow, events.ToList());
+
+            return events.ToList();
+
+        }
+        
+    }
+}
